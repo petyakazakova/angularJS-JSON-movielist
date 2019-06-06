291020cdf52a3d144b751a07d69d4bcedc2eaa0c
Petya Kazakova <pk.kazakova@gmail.com>
2019-06-06 09:11:53 +0200

    verson2

--- controller.js
+++ controller.js
@@ -1,8 +1,26 @@
 angular.module('MovieApp', [])
-  .controller('MovieController', ['$scope', 'MovieFactory',  function($scope, MovieFactory) {
+  .controller('MovieController', ['$scope', 'MovieFactory', function($scope, MovieFactory) {
 
-    //Call MovieFactory get function
     MovieFactory.get().then(function(response) {
+
+      $scope.movies = response.data;
+
+      // extract numbers from string
+      // $scope.duration = response.data.duration;
+      $scope.duration = 'PT150M'; //dummy
+      $scope.regex = /\d+/g;
+      $scope.found = parseInt($scope.duration.match($scope.regex)[0]);
+
+      // convert number to hours:min
+      function timeConvert(n) {
+        $scope.num = n;
+        $scope.hours = ($scope.num / 60);
+        $scope.rhours = Math.floor($scope.hours);
+        $scope.minutes = ($scope.hours - $scope.rhours) * 60;
+        $scope.rminutes = Math.round($scope.minutes);
+        return $scope.finalResult = $scope.num + " min = " + $scope.rhours + " h and " + $scope.rminutes + " min.";
+      }
+      timeConvert($scope.found);
 
     });
 
